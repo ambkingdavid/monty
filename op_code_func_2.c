acd0aa97173b6c508e54817bdc66cfd60fb01a23
Achebe Chika Luke <prosperbinary@gmail.com>
2023-01-20 08:50:06 -0800

    Monty Project

--- op_code_func_2.c
+++ op_code_func_2.c
@@ -1,11 +1,21 @@
 #include "monty.h"
 
+/**
+ * monty_nop - A script for nop
+ * @stack: Stack Argument
+ * @line_number: Number Argument
+ */
+
 void monty_nop(stack_t **stack, unsigned int line_number)
 {
 	(void) stack;
 	(void) line_number;
 }
-
+/**
+ * monty_pop - A script that inserts stack in monty function
+ * @stack: Stack Argument
+ * @line_number: Number Argument
+ */
 void monty_pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *node1, *node2;
