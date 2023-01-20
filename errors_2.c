abcbc7e8b0216c80dd4b49da93ddb08d9a93e60a
Achebe Chika Luke <prosperbinary@gmail.com>
2023-01-20 09:13:33 -0800

    Monty Project

--- errors_2.c
+++ errors_2.c
@@ -1,11 +1,17 @@
 #include "monty.h"
-
+/**
+ * swap_error - A function that prints swap error
+ * @line_number: Number Argument
+ */
 void swap_error(unsigned int line_number)
 {
 	fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 	exit(EXIT_FAILURE);
 }
-
+/**
+ * add_error - A function that prints add error
+ * @line_number: Number Argument
+ */
 void add_error(unsigned int line_number)
 {
 	fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
