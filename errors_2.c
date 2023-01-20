fb76ad6e1d51997747dbdfe0c8e86534432a45e7
Achebe Chika Luke <prosperbinary@gmail.com>
2023-01-20 01:31:50 -0800

    Monty project

--- errors_2.c
+++ errors_2.c
@@ -12,6 +12,12 @@ int swap_error(unsigned int line_number)
 	return (EXIT_FAILURE);
 }
 
+/**
+ * add_error - a script that swap monty error
+ * @line_number: number argument
+ *
+ * Return: EXIT_FAILURE
+ */
 int add_error(unsigned int line_number)
 {
 	fprintf(stderr, "L%d: can't add, stack too short", line_number);
