183062702a2dacb2f9c8cb48d2e25aff0b47227a
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-01-22 19:15:53 +0100

    add pchar

--- errors_3.c
+++ errors_3.c
@@ -6,3 +6,10 @@ void mod_error(stack_t **list, unsigned int line_number)
 	free_list(list);
 	exit(EXIT_FAILURE);
 }
+
+void add_error(stack_t **list, unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+	free_list(list);
+	exit(EXIT_FAILURE);
+}
--- op_code_func_3.c
+++ op_code_func_3.c
@@ -21,3 +21,24 @@ void monty_mod(stack_t **stack, unsigned int line_number)
 	(*stack) = node2;
 	free(node1);
 }
+
+
+
+void monty_pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node1;
+
+	if ((*stack) == NULL)
+	{
+		pchar_error(stack, line_number);
+	}
+	node1 = (*stack);
+	if (node1->n > 127 || node1->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range", line_number);
+		free_list(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", node1->n);
+}
