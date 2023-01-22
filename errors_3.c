467358d326e96b9dc1fe4958f4d3fbb04625c381
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-01-22 19:18:02 +0100

    add pchar

--- errors_3.c
+++ errors_3.c
@@ -7,7 +7,7 @@ void mod_error(stack_t **list, unsigned int line_number)
 	exit(EXIT_FAILURE);
 }
 
-void add_error(stack_t **list, unsigned int line_number)
+void pchar_error(stack_t **list, unsigned int line_number)
 {
 	fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 	free_list(list);
